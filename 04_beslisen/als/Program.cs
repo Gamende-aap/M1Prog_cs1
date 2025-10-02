673d1306144f5fde1868075937acdfd1ff916d70
Tygo <40080@ma-web.nl>
2025-10-02 10:27:19 +0200

    week 4

---
+++ 04_beslisen/als/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace als;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Console.WriteLine("Je zoekt je sleutel");
+
+
+        bool sleutelKwijt = true; 
+
+
+        if (sleutelKwijt)
+        {
+            Console.WriteLine("Je bent je sleutel kwijt!");
+            Console.WriteLine("De deur blijft gesloten");
+        }
+    }
+}
