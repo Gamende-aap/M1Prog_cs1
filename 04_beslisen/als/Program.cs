01e40fed1cbf422ce179cd83752a4fb87bf78124
Tygo <40080@ma-web.nl>
2025-10-02 10:48:42 +0200

    && bool

--- 04_beslisen/als/Program.cs
+++ 04_beslisen/als/Program.cs
@@ -1,20 +1,27 @@
-﻿namespace als;
+﻿using System.Globalization;
+
+namespace als;
 
 class Program
 {
     static void Main(string[] args)
     {
 
-        Console.WriteLine("Je zoekt je sleutel");
+        Console.WriteLine("Je zoekt de sleutel");
+        Console.WriteLine("Heb je de sleutel gevonden? (Y/N)");
 
-
-        bool sleutelKwijt = true; 
+        string input = Console.ReadLine();
+        bool sleutelKwijt = input == "N" || "n" == input;
 
 
         if (sleutelKwijt)
         {
             Console.WriteLine("Je bent je sleutel kwijt!");
             Console.WriteLine("De deur blijft gesloten");
+        }
+        else
+        {
+            Console.WriteLine("De deur gaat open je kan naar binnen");
         }
     }
 }
