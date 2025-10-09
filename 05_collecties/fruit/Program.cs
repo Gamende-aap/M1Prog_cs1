b898b8a85848e56d52fd6273dd6698147ee5270b
Tygo <40080@ma-web.nl>
2025-10-09 09:59:14 +0200

    arrays

---
+++ 05_collecties/fruit/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace fruit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] fruit = new string[2] { "passievrucht", "kers" };
+            string[] groente = new string[2];
+
+            int[] geluksgetallen = new int[] { 1, 4, 7 };
+            int[] totTien = new int[10];
+
+        }
+    }
+}
