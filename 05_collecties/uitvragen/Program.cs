a62fa9fac705bc940170c7181ae517daf855c93c
Tygo <40080@ma-web.nl>
2025-10-09 10:22:42 +0200

    uitvragen

---
+++ 05_collecties/uitvragen/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace uitvragen;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double[] percentages = new double[5] { .05, .10, .15, .20, .25 };
+
+        double temp = percentages[3];
+        temp = percentages[0];
+        Console.WriteLine(temp);
+        temp = percentages[1];
+        Console.WriteLine(temp);
+        temp = percentages[2];
+        Console.WriteLine(temp);
+        temp = percentages[3];
+        Console.WriteLine(temp);
+        temp = percentages[4];
+        Console.WriteLine(temp);
+
+
+ 
+    }
+}
