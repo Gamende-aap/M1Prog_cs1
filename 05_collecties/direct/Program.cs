577ee2c26c6579853a4c5eb6935a792dbd0e27a9
Tygo <40080@ma-web.nl>
2025-10-09 10:31:43 +0200

    direct

---
+++ 05_collecties/direct/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace direct;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double[] precentages = new double[] { 1.42, 2.41, 3.74, 4.82, 5.813, 6,125 };
+        Console.WriteLine($"eerste item in de array op de index 0 heeft de waarde {precentages[0]}");
+        Console.WriteLine($"eerste item in de array op de index 1 heeft de waarde {precentages[1]}");
+        Console.WriteLine($"eerste item in de array op de index 2 heeft de waarde {precentages[2]}");
+        Console.WriteLine($"eerste item in de array op de index 3 heeft de waarde {precentages[3]}");
+        Console.WriteLine($"eerste item in de array op de index 4 heeft de waarde {precentages[4]}");
+        Console.WriteLine($"eerste item in de array op de index 5 heeft de waarde {precentages[5]}");   
+    }
+}
