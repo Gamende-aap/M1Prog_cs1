8518acafe15d93b02a784b24573ef0434e88bb09
Tygo <40080@ma-web.nl>
2025-10-16 10:30:02 +0200

    stringclass

---
+++ 06_loops/stringclass/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace stringclass;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] geenCaps = { "Global", "Variable", "class" };
+        
+
+        for (int i = 0; i < geenCaps.Length; i++)
+        {
+        Console.WriteLine(geenCaps[i]);
+        Console.WriteLine($"Is een Variable die " + geenCaps[i].Length + " lang is");
+        Console.WriteLine(geenCaps[i].ToUpper()); 
+         
+        }
+    }
+}
