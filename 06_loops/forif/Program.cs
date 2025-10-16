c747c98d4e03ab5584880ef9d9cdb1ac072173d5
Tygo <40080@ma-web.nl>
2025-10-16 10:12:43 +0200

    forif

---
+++ 06_loops/forif/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace forif;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] Nummers = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        for (int i = 3; i < Nummers.Length -2; i++)
+        {
+            Console.WriteLine($"{Nummers[i]}");
+        }
+    }
+}
