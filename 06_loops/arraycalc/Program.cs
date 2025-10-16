fd45ff1373f888750f44c8e5e168837cceb287f9
Tygo <40080@ma-web.nl>
2025-10-16 09:57:06 +0200

    arraycalc

---
+++ 06_loops/arraycalc/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace arraycalc;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double[] Saldo = new double[]{240.99, 1142.23, 789.89, 658.72};
+        for (int i = 0; i < Saldo.Length; i++)
+        {
+            Console.WriteLine($"{i} {Saldo[i]}");
+        }
+        
+        double[] SaldoNa = new double[]{240.99 + 100, 1142.23 + 100, 789.89 + 100, 658.72 + 100};
+         for (int i = 0; i < SaldoNa.Length; i++)
+        {
+            Console.WriteLine($"{i} Saldo na verhooging {SaldoNa[i]}");
+         }
+    }
+}
