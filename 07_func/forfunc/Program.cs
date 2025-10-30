95d1a97be05c0e35dfdc6843dd17dcb91797d593
Tygo <40080@ma-web.nl>
2025-10-30 10:25:49 +0100

    forfunc

---
+++ 07_func/forfunc/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace forfunc;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double[] basis = { 2, 4, 6, 8 };
+
+        foreach (double getal in basis)
+        {
+            double temp = getal;
+
+            double resultaat = Math.Pow(temp, 2);
+
+            Console.WriteLine($"{temp} tot de macht 2 is {resultaat}");
+        }
+    }
+}
