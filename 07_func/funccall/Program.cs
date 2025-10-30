a76a8d824d4a003020a321e257c2788b49800b64
Tygo <40080@ma-web.nl>
2025-10-30 10:02:32 +0100

    Funccall

---
+++ 07_func/funccall/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace funccall;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.Clear();
+
+        Console.Write("....X....");
+
+        Console.SetCursorPosition(4, 0);
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.Write("?");
+        Console.ResetColor();
+        Console.WriteLine();
+
+        Console.WriteLine("X Was nothing");
+        Console.WriteLine();
+        Console.WriteLine("De ? hierboven is rood!");
+
+    }
+}
