ee8a1aac277aa46df0c1e6c4171476aec1385ba7
Tygo <40080@ma-web.nl>
2025-10-30 10:22:00 +0100

    math

---
+++ 07_func/math/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace math;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double kleiner = 9;
+        double groter = 19;
+        double welkeIsKleiner = Math.Min(kleiner, groter);
+        Console.WriteLine($"{welkeIsKleiner} is de kleinste van {kleiner} en {groter}");
+
+        double welkeIsGroter = Math.Max(kleiner, groter);
+        Console.WriteLine($"{welkeIsGroter} is de grootste van {kleiner} en {groter}");
+
+        double waarde = 16;
+        double min = 20;
+        double max = 30;
+        double beperkt = Math.Clamp(waarde, min, max);
+        Console.WriteLine($"Het clamp resultaat van {waarde} met min {min} en max {max} is {beperkt}");
+
+        double getal = 16;
+        double wortel = Math.Sqrt(getal);
+        Console.WriteLine($"De wortel van {getal} is {wortel}");
+    }
+}
